26499826863510a12734fd4275a54f7b999984da
fabioFgc7 <99999716+fabioFgc7@users.noreply.github.com>
2023-08-28 19:17:21 -0400

    Se agrego gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/book_lists_tecnical_test/'
+  base:"/book_lists_tecnical_test/"
 
 })
